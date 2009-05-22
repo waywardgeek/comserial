83c712da497b324a94ffbae9dc779a68b7784e99
smilindog2000 <smilindog2000@b8af2f9c-abda-45ab-a4ab-23c7217025c4>
2009-05-22 12:23:43 +0000

    /home/bill/projects/comserial: updates
    git-svn-id: svn://svn.code.sf.net/p/comserial/code/trunk@11 b8af2f9c-abda-45ab-a4ab-23c7217025c4

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
-all: login.cgi
+all: login.cgi client server
 
 login.cgi: login_cgi.c cgi_util.c cgi_util.h
 	gcc login_cgi.c cgi_util.c -o login.cgi
 
+server: server.c
+	gcc server.c -o server
 
+client: client.c
+	gcc client.c -o client
---
+++ comserial.c
@@ -0,0 +1,65 @@
+/*=================================================================================================
+  This program creates two file sockets.  The first is for communication with multiple clients,
+  typically Apache scritps responding to an HTTP request.  The second is a merged text stream
+  of all the communication with all of the clients.  This allows a server application to simply
+  communicate with the one server socket, much like it would if it were a simple console app.
+  Communication between server and client is done one line at a time, and the client's sessionId
+  is the first word of each line, followed by one space, and then the load of the message.
+=================================================================================================*/
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdio.h>
+
+struct clientStruct;
+typedef unsigned int uint;
+typedef struct clientStruct *client;
+
+struct clientStruct {
+    int socketdf;
+    char *lineBuf;
+    uint lineSize;
+    uint linePos;
+    char *sessionId;
+    uint hash;
+    client next;
+};
+
+static client *clientTable;
+static uint clientTableSize = 32;
+
+int main(
+    int argc,
+    char **argv)
+{
+    int         server_sockfd;
+    int         client_sockfd;
+    int         server_len;
+    int         client_len;
+    struct sockaddr_un server_address;
+    struct sockaddr_un client_address;
+
+    unlink("server_socket");
+    server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    server_address.sun_family = AF_UNIX;
+    strcpy(server_address.sun_path, "server_socket");
+    server_len = sizeof(server_address);
+    client_len = sizeof(client_address); //get byte length of client address
+    bind(server_sockfd, (struct sockaddr*)&server_address, server_len);
+
+    listen(server_sockfd, 5);
+    for(;;) {
+        char ch;
+
+        // printf("server waiting\n");
+
+        client_sockfd = accept(server_sockfd, (struct sockaddr*)&client_address, &client_len);
+
+        read(client_sockfd, &ch, 1);
+        ++ch;
+        write(client_sockfd, &ch, 1);
+        close(client_sockfd);
+    }
+}
