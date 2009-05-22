93ed9383b0d73ddf55c39b788af7963eb1843114
smilindog2000 <smilindog2000@b8af2f9c-abda-45ab-a4ab-23c7217025c4>
2009-05-22 11:56:03 +0000

    update
    git-svn-id: svn://svn.code.sf.net/p/comserial/code/trunk@10 b8af2f9c-abda-45ab-a4ab-23c7217025c4

--- example_backend.c
+++ example_backend.c
@@ -3,11 +3,59 @@
 =================================================================================================*/
 #include <stdio.h>
 
-FILE *file;
+static FILE *file;
+static char *line;
+static int lineLength = 42;
+
+/*=================================================================================================
+  Read one line from the file.  Return false if there is no data.
+=================================================================================================*/
+static int readLine(void)
+{
+    int linePos = 0;
+    int c = getc(file);
+
+    while(c != EOF && c != '\n') {
+	if(c >= ' ') {
+	    /* Ignore newlines returns and tabs */
+	    if(linePos + 1 == lineLength) {
+		lineLength <<= 1;
+		line = (char *)realloc(line, lineLength*sizeof(char));
+	    }
+	    line[linePos++] = c;
+	}
+    }
+    line[linePos] = '\0';
+    return linePos > 0;
+}
+
+/*=================================================================================================
+  Execute the command in the line variable.  The first word is always the user's sessioId.
+  To help keep track of what session we're responding to, print "Start <sessionId>" in the
+  beginning of the response, and "Finish <sessionId>" at the end.
+=================================================================================================*/
+static void executeCommand(void)
+{
+    char *sessionId = readWord();
+    char *command = readWord();
+
+    printf("Start %s\n", sessionId);
+    if(!strcmp(command, "login")) {
+	if(!strcmp(readWord(), "bill") && !strcmp(readWord(), "test")) {
+	    printf("Logic successful\n");
+	}
+    }
+    printf("Finish %s\n", sessionId);
+}
 
 static void executeFileCommands(void)
 {
-
+    while(1) {
+	if(!readLine()) {
+	    return;
+	}
+	executeCommand();
+    }
 }
 
 int main(
@@ -18,6 +66,7 @@ int main(
 	printf("Usage: %s commandFile\n", argv[0]);
 	return 1;
     }
+    line = (char *)calloc(lineLength, sizeof(char));
     file = fopen(argv[1], "r");
     if(file == NULL) {
 	printf("Unable to open %s for reading\n", argv[1]);
