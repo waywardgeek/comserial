3c8d459d1b0931dbf306717f289c1f094b79b352
smilindog2000 <smilindog2000@b8af2f9c-abda-45ab-a4ab-23c7217025c4>
2009-05-22 13:04:29 +0000

    /home/bill/projects/comserial: updates
    git-svn-id: svn://svn.code.sf.net/p/comserial/code/trunk@12 b8af2f9c-abda-45ab-a4ab-23c7217025c4

--- comserial.c
+++ comserial.c
@@ -1,10 +1,50 @@
 /*=================================================================================================
-  This program creates two file sockets.  The first is for communication with multiple clients,
-  typically Apache scritps responding to an HTTP request.  The second is a merged text stream
-  of all the communication with all of the clients.  This allows a server application to simply
-  communicate with the one server socket, much like it would if it were a simple console app.
-  Communication between server and client is done one line at a time, and the client's sessionId
-  is the first word of each line, followed by one space, and then the load of the message.
+  This is a server-side module which creates a file socket for communication with multiple
+  clients, typically Apache scripts responding to an HTTP request.  A simple printf/getc style
+  interface is presented to the server which merges all client communication into one input and
+  one output stream.  The point is to allow console apps to serve as the back-end engines to
+  Apache without the hassle of managing network connections, multiple threads, or 'select' loops.
+  Communication between server and client is done one command at a time.
+
+  All messages between client and server are of the form:
+
+      sessionId &text message&
+
+  The sessionId is any string not containing spaces, and is terminated by a single space.  The
+  text message is encoded such that ampersands are replaced with % escapes.  However, the user
+  does not need to worry about message format, because the server and client communicate through
+  higher level functions.
+
+  The client communicates with the server using:
+
+      coSendMessage(char *sessionId, char *format, ...);
+      char *coReadMessage(char **sessionIdPtr);
+
+  The server reacts to client messages with:
+
+      char *coStartResponse(void);
+      int coPrint(char *format, ...);
+      coCompleteResponse(void);
+      char coGetc(void):
+
+  The point of these functions is to act more like the functions used in a typical console app:
+  printf and getchar.  At the top level of a command interpreter loop, you should call
+  coStartResponse, which returns the sessionId of the next message from a client.  Then call your
+  command processing code, which calls coGetc until it returns '\n' ('\0' will be the actual
+  termination of the messsage).  Instead of calling printf in response, call coPrint.  When
+  control returns back to the command processing loop, call coCompleteResponse to cause the
+  response message to be sent.  Note that empty responses will result in an empty message, so
+  clients should expect a response to every message.
+
+  The server and initialize/closesthings with:
+
+      int coStartServer(char *fileSocketPath);
+      void coStopServer(void);
+
+  The client does so with:
+
+      int coStartClient(char *fileSocketPath);
+      void coStopClient(void);
 =================================================================================================*/
 #include <sys/types.h>
 #include <sys/socket.h>
