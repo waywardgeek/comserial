c975266371bf5c58c4f0ca5bc5fe5db99d3c79d8
smilindog2000 <smilindog2000@b8af2f9c-abda-45ab-a4ab-23c7217025c4>
2010-07-14 22:57:55 +0000

    Added benchmark.c
    git-svn-id: svn://svn.code.sf.net/p/comserial/code/trunk@43 b8af2f9c-abda-45ab-a4ab-23c7217025c4

---
+++ benchmark.c
@@ -0,0 +1,37 @@
+/*--------------------------------------------------------------------------------------------------
+  This is just a test client, which let's you directly type to your console application.
+-------------------------------------------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cgiutil.h"
+#include "comclient.h"
+
+#define NUM_LOOPS 1000000
+
+int main(
+    int argc,
+    char **argv)
+{
+    char *response;
+    char *sessionID = cgiGenerateRandomID(20);
+    int i;
+
+    if(argc != 2) {
+        printf("Usage: %s fileSocketPath\n", argv[0]);
+        return 1;
+    }
+    response = coStartClient(argv[1], sessionID);
+    for(i = 0; i < NUM_LOOPS; i++) {
+	if(i & 1) {
+	    coSendMessage("login admin donttell");
+	    response = coReadMessage();
+	} else {
+	    coSendMessage("logout");
+	    response = coReadMessage();
+	}
+    }
+    coStopClient();
+    return 0;
+}
+
